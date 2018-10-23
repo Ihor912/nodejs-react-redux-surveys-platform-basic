cee208faf737636b970433e34e233cae569e0351
Ihor Khomiak <iokhomiak@gmail.com>
2018-10-23 18:48:31 +0300

    implement functionality on client

---
+++ client/src/components/Header.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+class Header extends Component {
+    renderContent() {
+        switch (this.props.auth) {
+            case null:
+                return;
+            case false:
+                return <a href="/auth/google">Login With Google</a>;
+            default:
+                return <a href="/api/logout">Logout</a>;
+        }
+    }
+
+    render() {
+        return(
+            <nav>
+                <div className="nav-wrapper">
+                    <Link to={this.props.auth ? '/surveys' : '/'} className="offset-s6 left brand-logo">
+                        Emaily
+                    </Link>
+                    <ul className="right">
+                        <li>{this.renderContent()}</li>
+                    </ul>
+                </div>
+            </nav>
+        );
+    }
+}
+
+function mapStateToProps({ auth }) {
+    return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
---
+++ client/src/components/Landing.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Landing extends Component {
+    render() {
+        return(
+            <div style={{ textAlign: 'center' }}>
+                <h1>Survey Services</h1>
+                Collect feedback form your users
+            </div>
+        );
+    }
+}
+
+export default Landing;
