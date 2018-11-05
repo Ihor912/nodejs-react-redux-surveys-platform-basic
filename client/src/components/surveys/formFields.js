566ccc509761fe199e9d052e4edad00925c90536
Ihor Khomiak <iokhomiak@gmail.com>
2018-11-05 11:16:10 +0200

    implement create survey functionality on client

---
+++ client/src/components/surveys/formFields.js
@@ -0,0 +1,6 @@
+export default [
+    { label: 'Campaign Title', name: 'title' },
+    { label: 'Subject Line', name: 'subject' },
+    { label: 'Body', name: 'body' },
+    { label: 'Recipient Email List', name: 'recipients' },
+];
